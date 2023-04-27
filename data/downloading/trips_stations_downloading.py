348c8071ad6269f0d5d2007e9ac757451da13ebb
Erika Gutierrez <erikagutierrez@ciutadella-134-62.eduroamcc.upf.edu>
2023-04-27 17:50:40 +0200

    code to bulk download trip and station data  for specified date range

---
+++ data/downloading/trips_stations_downloading.py
@@ -0,0 +1,69 @@
+import pandas as pd
+import re
+import requests
+import os
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager #details: https://pypi.org/project/webdriver-manager/
+from dateutil import rrule
+from datetime import datetime
+
+data_dl_path = '/stations_raw'
+
+start_date = datetime(2018, 7, 1)
+end_date = datetime(2021, 6, 30)
+
+date_range = list(rrule.rrule(rrule.MONTHLY, dtstart=start_date, until=end_date))
+
+months = []
+years = []
+for date in date_range:
+    months.append(date.strftime("%B"))
+    years.append(date.strftime("%Y"))
+
+months_translator = {'January':'enero',
+                     'February': 'febrero',
+                     'March': 'marzo',
+                     'April': 'abril',
+                     'May': 'mayo',
+                     'June': 'junio',
+                     'July': 'julio',
+                     'August':'agosto',
+                     'September':'septiembre',
+                     'October': 'octubre', 
+                     'November': 'noviembre',
+                     'December': 'diciembre'}
+
+months = [months_translator[month] for month in months]
+
+
+options = webdriver.ChromeOptions()
+prefs = {"download.default_directory" : data_dl_path}
+options.add_experimental_option("prefs",prefs)
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+driver.get('https://opendata.emtmadrid.es/Datos-estaticos/Datos-generales-(1)')
+
+
+links = driver.find_elements(By.TAG_NAME, 'a')
+
+hrefs= []
+titles = []
+for link in links:
+    link_title = link.get_attribute('title').lower()
+    if link.get_attribute('target') == '_blank' and 'enlace' not in link_title:
+        for i in range(len(months)):
+            if f'{months[i]} {years[i]}' in link_title or f'{months[i]} de {years[i]}' in link_title:
+               hrefs.append(link.get_attribute('href'))
+               titles.append(link_title)
+               link.click()
+
+print(hrefs)
+print(titles)
+
+
+
+
+
