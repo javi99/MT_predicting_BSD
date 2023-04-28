5faca9647244ddad157a1ef184cb1c81d4646af6
Erika Gutierrez <erikagutierrez@Erikas-MacBook-Pro.local>
2023-04-28 11:39:14 +0200

    Editing logic for unzipping files

--- data/downloading/trips_stations_downloading.py
+++ data/downloading/trips_stations_downloading.py
@@ -11,7 +11,9 @@ from dateutil import rrule
 from datetime import datetime
 import zipfile
 
-data_dl_path = os.path.join(os.path.dirname(__file__), 'stations_raw')
+#data_dl_path = os.path.join(os.path.dirname(__file__), 'stations_raw')
+data_dl_path = os.getcwd()+'/data/downloading/stations_raw'
+
 
 start_date = datetime(2018, 7, 1)
 end_date = datetime(2021, 6, 30)
@@ -63,9 +65,12 @@ for link in links:
                link.click()
                time.sleep(5)
 
-time.sleep(10)
+time.sleep(30)
 
 driver.close()
+
+
+# move file name to dl file name match to into for loop?
 
 print(hrefs)
 print(titles)
@@ -74,3 +79,50 @@ current_time = datetime.now().time()
 pd.DataFrame({'title':titles, 'links':hrefs}).to_csv(f'{data_dl_path}/download_{current_time}.csv')
 
 # unzip files
+zip_path = "/path/to/archive.zip"
+
+# Specify the path to the directory where files will be extracted
+extract_path = "/path/to/extract"
+
+#match files to their href
+
+dl_files = []
+for href in hrefs:
+    p = re.search('(?<=\/)[^\/]+?(?=\.aspx)', href)
+    dl_files.append(p.group(0))
+
+files_dl = os.listdir(data_dl_path)
+
+
+
+
+def ziporrar(file):
+    if '.zip' in file or '.rar' in file:
+        return True
+    else:
+        return False
+
+def unzipper(files):
+    for file in files:
+        if ziporrar(file):
+            print('ok')
+
+
+    # 1. check if file is .zip or .rar or other
+    # 2. unzip file and put contents into extract folder (same name as title?)
+    # 3. delete .zip and .rar files
+
+
+
+
+'''
+# Create the extract directory if it doesn't exist
+if not os.path.exists(extract_path):
+    os.makedirs(extract_path)
+
+
+# Open the zip archive for reading
+with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+    # Extract all files to the extract directory
+    zip_ref.extractall(extract_path)
+'''
